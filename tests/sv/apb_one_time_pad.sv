f986e778eddab6d1dc5682c6be6165658d438dd9
Benjamin Davis <benjamin.j.davis96@gmail.com>
2024-03-01 12:17:09 -0700

    Working one-time-pad

--- tests/sv/apb_one_time_pad.sv
+++ tests/sv/apb_one_time_pad.sv
@@ -1,23 +1,54 @@
 
 module apb_one_time_pad #(parameter int WIDTH = 128) (
-    input logic clk,
-    input logic rst_n,
-    input logic [31:0] addr,
-    input logic [WIDTH-1:0] wdata,
-    input logic we,
-    output logic [WIDTH-1:0] rdata
+    input logic pclk,
+    input logic preset_n,
+    input logic [31:0] paddr,
+    input logic psel,
+    input logic penable,
+    input logic pwrite,
+    input logic [WIDTH-1:0] pwdata,
+    output logic [WIDTH-1:0] prdata,
+    output logic pready
 );
 
-    logic [WIDTH-1:0] mem [0:255];
+    logic [WIDTH-1:0] key;
+    logic [WIDTH-1:0] data;
+    logic [WIDTH-1:0] result;
 
-    always_ff @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            mem <= '{default:0};
-        end else if (we) begin
-            mem[addr] <= wdata;
+    always_ff @(posedge pclk or negedge preset_n) begin
+        if (!preset_n) begin
+            key <= 0;
+            data <= 0;
+        end
+        else begin
+            if (psel && penable && (paddr == 'h0) && pwrite) begin
+                key <= pwdata;
+            end
+            else if (psel && penable && (paddr == 'h1) && pwrite) begin
+                data <= pwdata;
+            end
         end
     end
 
     always_comb begin
-        rdata = mem[addr];
+        result = data ^ key;
     end
+
+    always_ff @(posedge pclk or negedge preset_n) begin
+        if (!preset_n) begin
+            prdata <= 0;
+            pready <= 0;
+        end
+        else begin
+            if (psel && penable && !pwrite) begin
+                pready <= 1;
+                if (paddr == 'h2) begin
+                    prdata <= result;
+                end
+                else begin
+                    prdata <= 0;
+                end
+            end
+        end
+    end
+endmodule
