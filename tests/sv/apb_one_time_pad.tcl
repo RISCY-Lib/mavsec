57ec0c7d51bf50fbb03ea978d64da265382c7c09
Benjamin Davis <benjamin.j.davis96@gmail.com>
2024-03-03 14:43:39 -0700

    Updated expected tcl output

--- tests/sv/apb_one_time_pad.tcl
+++ tests/sv/apb_one_time_pad.tcl
@@ -1,14 +1,32 @@
 clear -all
 check_spv -init
 
-analyze -sv test_modules/apb_one_time_pad.sv
-elaborate -top apb_one_time_pad
+# Set up the design
+####################################################################################################
+#  This file needs to:
+#  - Analyze (i.e. parse) the design
+#  - Elaborate (i.e. compile) the design
+#  - Set the Clock and Resets to the design
+source ./jg_setup.tcl
 
-clock pclk
-reset ~preset_n
-
+# OTP Key Property
+####################################################################################################
 foreach output [get_design_info -list output] {
-    check_spv -create -name key_to_${output}_instr -from key -to $output -to_precond pprot\[2\]
-    check_spv -create -name key_to_${output}_insecure -from key -to $output -to_precond pprot\[1\]
+    check_spv -create -name otp_key_${output}_precond0 -from key -to $output -to_precond pprot\[2\]==0
+    check_spv -create -name otp_key_${output}_precond1 -from key -to $output -to_precond pprot\[1\]==0
 }
+
+# OTP Key Integrity Property
+####################################################################################################
+foreach input [get_design_info -list input] {
+    check_spv -create -name otp_key_${input}_precond0 -from $input -to key -to_precond pprot\[0\]==0
+}
+
+# OTP Plaintext Property
+####################################################################################################
+foreach output [get_design_info -list output] {
+    check_spv -create -name otp_plaintext_${output} -from plaintext -to $output
+    check_spv -create -name otp_plaintext_${output} -from plaintext -to $output
+}
+
 check_spv -prove
